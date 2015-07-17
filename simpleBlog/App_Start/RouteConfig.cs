dc4b0deefee332e1868c430a9733e3d0f134b594
JonatasC <jonataschizzolini@gmail.com>
2015-07-17 15:20:11 -0300

    arrumei

--- simpleBlog/App_Start/RouteConfig.cs
+++ simpleBlog/App_Start/RouteConfig.cs
@@ -12,13 +12,13 @@ namespace simpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var namespaces = new[] { typeof(PostsController).Namespace };
+            var namespaces = new string[] { typeof(PostsController).Namespace };
             
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(name: "Login", url: "login", defaults: new { controller = "Auth", action = "Login" },namespaces);
-            
-            routes.MapRoute(name: "Home", url: "", defaults: new { controller = "Posts", action = "Index"},namespaces);
+            routes.MapRoute(name: "Login", url: "login", defaults: new { controller = "Auth", action = "Login" }, namespaces: namespaces);
+
+            routes.MapRoute(namespaces: namespaces, name: "Home", url: "", defaults: new { controller = "Posts", action = "Index" });
         }
     }
 }
